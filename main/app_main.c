90a9b3ed3a71726732254f3d01da8af5a0ae1ac4
NguyenTanPhat2002 <phatnguyen3102002@gmail.com>
2026-06-20 08:47:44 +0700

    ADD wifi

--- main/app_main.c
+++ main/app_main.c
@@ -52,7 +52,11 @@
 #define WIFI_PASS "Modern2025@1"
 
 #define WIFI_MAX_RETRY  5
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT   BIT1
 
+static EventGroupHandle_t s_wifi_event_group;
+static int s_retry_num =0;
 
 static TimerHandle_t detect_timer = NULL;
 static bool sensor_confirmed = false;
@@ -73,6 +77,83 @@ typedef struct
 static QueueHandle_t tx_frame_queue = NULL;
 static QueueHandle_t sensor_event_queue = NULL;
 static QueueHandle_t gpio_evt_queue = NULL;
+
+static void event_handler(void *arg, esp_event_base_t event_base, 
+                            int32_t event_id, void *event_data)
+{
+    if(event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
+    {
+        esp_wifi_connect();
+    }
+
+    else if(event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
+    {
+        if(s_retry_num < WIFI_MAX_RETRY)
+        {
+            esp_wifi_connect();
+            s_retry_num ++;
+            ESP_LOGI(TAG, "retry to connect wifi MODERN SYSTEM");
+        }
+        else{
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+        }
+        ESP_LOGI(TAG, "connect to the AP fail");
+    }
+
+    else if(event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
+    {
+        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num =0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
+void wifi_init_sta(void)
+{
+    s_wifi_event_group = xEventGroupCreate();
+    
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instace_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(
+        WIFI_EVENT,
+        ESP_EVENT_ANY_ID,
+        &event_handler,
+        NULL,
+        &instance_any_id
+    ));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(
+        IP_EVENT,
+        IP_EVENT_STA_GOT_IP,
+        &event_handler,
+        NULL,
+        &instance_got_ip
+    ));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASS,
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+
+    /* Wait until 1 or 2 this bit is setted: 
+        1. WIFI_CONNECTED_BIT
+        2. WIFI_FAIL_BIT */
+}
 
 static void log_hex_frame(const uint8_t *buf, size_t len)
 {
